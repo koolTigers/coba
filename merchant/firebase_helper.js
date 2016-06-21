d2688c20602966a18b10a6712aa8fc87be174f38
Gerardo Montoya <gerardo.montoya.v@gmail.com>
2016-06-20 22:45:15 -0500

    Add method to retrieve product with shop and barcode

--- merchant/firebase_helper.js
+++ merchant/firebase_helper.js
@@ -1,58 +1,77 @@
-		function insertProduct(name, description, category, type, unit, size, brand, barcode, photo, store) {
-			var productID = firebase.database().ref().child('products').push().key;
-			var product = {
-				name: name,
-				description: description,
-				category: category,
-				type: type,
-				unit: unit,
-				size: size,
-				brand: brand,
-				barcode: barcode,
-				photo: photo
-			}
-			var updates = {};
-			updates['/products/' + productID] = product
-			firebase.database().ref().update(updates)
-		}
-		function insertShop(name){
-			var shopID = firebase.database().ref().child('shops').push().key;
-			var shop = {
-				name: name
-			}
-			var updates = {};
-			updates['/shops/' + shopID] = shop
-			firebase.database().ref().update(updates)
-		}
-		function insertShopProduct(idProduct, idShop, price){
-			var productShop = firebase.database().ref().child('products-shop').push().key;
-			var product = {
-				price: price
-			}
-			var updates = {};
-			updates['/products-shop/' + idShop + '/' + idProduct + '/' + productShop] = product
-			firebase.database().ref().update(updates)
-		}
+function insertProduct(name, description, category, type, unit, size, brand, barcode, photo, store) {
+	var productID = firebase.database().ref().child('products').push().key;
+	var product = {
+		name: name,
+		description: description,
+		category: category,
+		type: type,
+		unit: unit,
+		size: size,
+		brand: brand,
+		barcode: barcode,
+		photo: photo
+	}
+	var updates = {};
+	updates['/products/' + productID] = product
+	firebase.database().ref().update(updates)
+}
+function insertShop(name){
+	var shopID = firebase.database().ref().child('shops').push().key;
+	var shop = {
+		name: name
+	}
+	var updates = {};
+	updates['/shops/' + shopID] = shop
+	firebase.database().ref().update(updates)
+}
+function insertShopProduct(idProduct, idShop, price){
+	var productShop = firebase.database().ref().child('products-shop').push().key;
+	var product = {
+		price: price
+	}
+	var updates = {};
+	updates['/products-shop/' + idShop + '/' + idProduct + '/' + productShop] = product
+	firebase.database().ref().update(updates)
+}
 
-		function getAllProducts(callback) {
-			//var ref = new firebase("https://coba-54f85.firebaseio.com/products");
-			ref = firebase.database().ref('products')
-			ref.on("value", function(snapshot) {
-				//console.log(snapshot.val())
-				callback(snapshot.val())
-			});
-		}
+function getAllProducts(callback) {
+//var ref = new firebase("https://coba-54f85.firebaseio.com/products");
+ref = firebase.database().ref('products')
+ref.on("value", function(snapshot) {
+//console.log(snapshot.val())
+callback(snapshot.val())
+});
+}
 
-		function getProductsByName(name, callback) {
-			ref = firebase.database().ref('products')
-			ref.orderByKey().startAt(name).on("value", function(snapshot) {
-				callback(snapshot.val())
-			});
-		}
+function getProductsByName(name, callback) {
+	ref = firebase.database().ref('products')
+	ref.orderByKey().startAt(name).on("value", function(snapshot) {
+		callback(snapshot.val())
+	});
+}
 
-		function getProductByBarcode(barcode, callback) {
-			ref = firebase.database().ref('products')
-			ref.orderByChild("barcode").equalTo(barcode).on("value", function(snapshot) {
-				callback(snapshot.val())
-			});
+function getProductByBarcode(barcode, callback) {
+	ref = firebase.database().ref('products')
+	ref.orderByChild("barcode").equalTo(barcode).on("value", function(snapshot) {
+		callback(snapshot.val())
+	});
+}
+
+function getProductByBarcode(barcode, callback) {
+	ref = firebase.database().ref('products')
+	ref.orderByChild("barcode").equalTo(barcode).on("value", function(snapshot) {
+		callback(snapshot.val())
+	});
+}
+
+function getProductByCodeAndShop(barcode, shop, callback) {
+	ref = firebase.database().ref('products')
+	ref.orderByChild("barcode").equalTo(barcode).on("value", function(snapshot) {
+		var product = snapshot.val()
+		for (var pr in product) {
+			if (product[pr].shop == shop) {
+				callback(product[pr])
+			}
 		}
+	});
+}
